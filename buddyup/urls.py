25d420752faa2fb159094a4982f827aae3ff928f
Stefania Tiganila <tiganilastefania23@gmail.com>
2026-05-09 01:09:21 +0300

    report and block feature

--- buddyup/urls.py
+++ buddyup/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('swipes/', include('swipes.urls')),
     path('matches/', include('matches.urls')),
     path('profiles/', include('profiles.urls')),
+    path('reports/', include('reports.urls')),
 ]
 
 urlpatterns += static(
